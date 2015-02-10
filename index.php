407bc27bee449cfaa839cd92a45d0a293832ddb0
FsMz <jerome.pipino@gmail.com>
2015-02-10 01:42:12 +0100

    pd3

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include("include/haut.php");
 			<div class="span10 offset1">
 
 			<br />
-			<p><img src="images/pizza_Yolo1.png" alt="Logo de AvionYolo" class="img-responsive" style="margin: 0 auto; max-width: 50%; max-height:50%;" /></p>
+			<p><img src="images/planetmarket.jpg" alt="Logo de Plan'etMarket" class="img-responsive" style="margin: 0 auto; max-width: 50%; max-height:50%;" /></p>
 			<h2>Le spécialiste de l'ePlane</h2>
 			
 			<p>Le magasin Plan'etMarket ne se situe pas, car il est partout! Première référence de l'ePlane français, aucune boutique n'est disponible. <br />
@@ -21,8 +21,6 @@ include("include/haut.php");
 			Nous sommes spécialisés dans la vente d'avion par correspondance. Ils sont personnalisables à souhait. <br />
 			Plan'etMarket c'est avant tout un service de qualité ainsi qu'une livraison qui brise le mur du son.
 			</p>
-
-			<p><img src="images/eplane.png" alt="EPlane" class="img-responsive" style="margin: 0 auto;" /></p>
 
 			</div>
 <?php
